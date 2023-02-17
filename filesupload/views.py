a73c4ea8495ee92f33bd788271067b2b066343bb
Vishalanshuman <vishalanshuman@gmail.com>
2023-02-17 19:08:23 +0530

    update

--- filesupload/views.py
+++ filesupload/views.py
@@ -1,8 +1,9 @@
 from . models import PhotoAlbum
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .forms import UploadFileForm
 from django.views.decorators.csrf import ensure_csrf_cookie
+
 
 @ensure_csrf_cookie
 def upload_file(request):
@@ -10,28 +11,30 @@ def upload_file(request):
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             handle_uploaded_file(request.FILES['file'])
-            context = {'msg' : '<span style="color: green;">File successfully uploaded</span>'}
+            context = {
+                'msg': '<span style="color: green;">File successfully uploaded</span>'}
             return render(request, "single.html", context)
     else:
         form = UploadFileForm()
     return render(request, 'single.html', {'form': form})
+
 
 def handle_uploaded_file(f):
     with open(f.name, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
 
+
 @ensure_csrf_cookie
 def upload_multiple_files(request):
     if request.method == 'POST':
         name = request.POST.get('name')
-        images = request.POST.getlist("images")
-        # files = request.FILES.getlist('files')
+        images = request.POST.getlist('images')
         for image in images:
-            photo=PhotoAlbum.objects.create(
-                name  = name,
-                images = image
-            )
-            photo.save()
-
+            photo= PhotoAlbum.objects.create(
+                    name=name,
+                    images=image
+                    )
+            # photo.save()
+        # return HttpResponse('Files uploaded')
     return render(request, 'multiple.html')
